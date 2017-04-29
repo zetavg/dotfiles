f99f296a4d4dbad94861aa53097fec53fd9f369f
zetavg <mail@zeta.vg>
2017-04-29 14:13:11 +0800

    update

--- _slate.js
+++ _slate.js
@@ -1,253 +1,237 @@
-/* Slate Config */
+/*
+ * Slate Config
+ */
+
 slate.configAll({
-  repeatOnHoldOps : "resize,nudge",
-  keyboardLayout : "qwerty",
-  windowHintsIgnoreHiddenWindows : false,
-  windowHintsSpread : true
+  repeatOnHoldOps: "resize,nudge",
+  secondsBeforeRepeat: 0.4,
+  secondsBetweenRepeat: 0.05,
+  keyboardLayout: "qwerty",
+  snapshotMaxStackSize: 1000,
+  undoMaxStackSize: 1000,
+  windowHintsIgnoreHiddenWindows: true,
+  windowHintsSpread: true,
 });
 
-/* Define Keys */
+/*
+ * Define Keys
+ */
+
 var hyperKey = 'cmd;shift';
 var optionKey = 'alt';
 
-/* Define Applications */
-var Chrome = 'Google Chrome';
-var Terminal = 'Terminal';
-var Terminal2 = '終端機';
-var Subl = 'Sublime Text';
+function withHyperKey(key, option) {
+  if (option) {
+    return key + ':' + hyperKey + ';' + optionKey;
+  } else {
+    return key + ':' + hyperKey;
+  }
+}
 
-/* Step */
+/*
+ * Define Applications
+ */
+
+var defaultApplications = {
+  webBrowser: 'Opera Developer',
+  terminal: 'iTerm',
+  textEditor: 'Sublime Text',
+};
+
+/*
+ * Helpers
+ */
+
+function getNextScreenID() {
+  var currentScreen = slate.screen();
+  var currentScreenID = currentScreen.id();
+  var nextScreenID = currentScreenID + 1;
+  if (nextScreenID >= slate.screenCount()) nextScreenID = 0;
+  slate.log(nextScreenID);
+  return nextScreenID.toString();
+}
+
+/*
+ * Define Step Logging Operations
+ */
+
 var logStep = slate.operation("snapshot", {
   "name" : "loggedSteps",
   "save" : true,
   "stack" : true
 });
+
 var stepBack = slate.operation("activate-snapshot", {
   "name" : "loggedSteps",
   "delete" : true,
 });
 
-/* Operations */
-// focus
-var hint = slate.operation("hint", {
-  "characters" : "JKHLUINMYOP"
-});
-// push to
-var pushRight = slate.operation("push", {
-  "direction" : "right",
-  "style" : "bar-resize:screenSizeX/2"
-});
-var pushLeft = slate.operation("push", {
-  "direction" : "left",
-  "style" : "bar-resize:screenSizeX/2"
-});
-var pushTop = slate.operation("push", {
-  "direction" : "top",
-  "style" : "bar-resize:screenSizeY/2"
-});
-var fullscreen = slate.operation("move", {
-  "x" : "screenOriginX",
-  "y" : "screenOriginY",
-  "width" : "screenSizeX",
-  "height" : "screenSizeY"
-});
-// move and resize
-var moveDown = slate.operation("nudge", {
-  "x" : "+0",
-  "y" : "+20"
-});
-var moveUp = slate.operation("nudge", {
-  "x" : "+0",
-  "y" : "-20"
-});
-var moveRight = slate.operation("nudge", {
-  "x" : "+20",
-  "y" : "+0"
-});
-var moveLeft = slate.operation("nudge", {
-  "x" : "-20",
-  "y" : "+0"
-});
-var moveAndResizeToCenter = slate.operation("move", {
-  "x" : "screenOriginX+screenSizeX/10",
-  "y" : "screenOriginY+screenSizeY/10",
-  "width" : "screenSizeX*0.8",
-  "height" : "screenSizeY*0.8"
-});
-var moveToCenter = slate.operation("move", {
-  "x" : "screenSizeX/2-windowSizeX/2",
-  "y" : "screenSizeY/2-windowSizeY/2",
-  "width" : "windowSizeX",
-  "height" : "windowSizeY"
-});
-var throwNext = slate.operation("throw", {
-  "screen" : 'next'
-});
-// grid
-var grid44 = slate.operation("grid", {
-  "grids" : {
-    "0" : {
-      "width" : 4,
-      "height" : 4
-    },
-    "1" : {
-      "width" : 4,
-      "height" : 4
-    },
-    "2" : {
-      "width" : 4,
-      "height" : 4
-    },
-    "3" : {
-      "width" : 4,
-      "height" : 4
-    },
-    "4" : {
-      "width" : 4,
-      "height" : 4
-    },
-    "5" : {
-      "width" : 4,
-      "height" : 4
-    }
-  },
-  "padding" : 5
-});
+/*
+ * Define Operations
+ */
 
+var DEFAULT_UNIT = 20;
 
-/* Bind Keys */
-// focus
-slate.bind('j:'+hyperKey, function(win) {
-  win.doOperation(slate.operation("focus", { "direction" : "down" }));
-}, true);
-slate.bind('k:'+hyperKey, function(win) {
-  win.doOperation(slate.operation("focus", { "direction" : "up" }));
-}, true);
-slate.bind('h:'+hyperKey, function(win) {
-  win.doOperation(slate.operation("focus", { "direction" : "left" }));
-}, true);
-slate.bind('l:'+hyperKey, function(win) {
-  win.doOperation(slate.operation("focus", { "direction" : "right" }));
-}, true);
-slate.bind('esc:cmd', hint);
-// move and resize
-slate.bind('j:'+hyperKey+';'+optionKey, function(win) {
-  win.doOperation(logStep);
-  win.doOperation(moveDown);
-}, true);
-slate.bind('k:'+hyperKey+';'+optionKey, function(win) {
-  win.doOperation(logStep);
-  win.doOperation(moveUp);
-}, true);
-slate.bind('h:'+hyperKey+';'+optionKey, function(win) {
-  win.doOperation(logStep);
-  win.doOperation(moveLeft);
-}, true);
-slate.bind('l:'+hyperKey+';'+optionKey, function(win) {
-  win.doOperation(logStep);
-  win.doOperation(moveRight);
-}, true);
-slate.bind('m:'+hyperKey+';'+optionKey, function(win) {
-  win.doOperation(logStep);
-  win.doOperation(slate.operation("resize", {
-    "width" : "+0",
-    "height" : "+40"
-  }));
-  win.doOperation(slate.operation("nudge", {
+var operations = {
+  // Move window
+  moveDown: slate.operation("nudge", {
     "x" : "+0",
-    "y" : "-20"
-  }));
-}, true);
-slate.bind(',:'+hyperKey+';'+optionKey, function(win) {
-  win.doOperation(logStep);
-  win.doOperation(slate.operation("resize", {
-    "width" : "+0",
-    "height" : "-40"
-  }));
-  win.doOperation(slate.operation("nudge", {
+    "y" : "+" + DEFAULT_UNIT,
+  }),
+  moveUp: slate.operation("nudge", {
     "x" : "+0",
-    "y" : "+20"
+    "y" : "-" + DEFAULT_UNIT,
+  }),
+  moveRight: slate.operation("nudge", {
+    "x" : "+" + DEFAULT_UNIT,
+    "y" : "+0",
+  }),
+  moveLeft: slate.operation("nudge", {
+    "x" : "-" + DEFAULT_UNIT,
+    "y" : "+0",
+  }),
+  // Resize window
+  increaseHeight: slate.operation("resize", {
+    "width" : "+0",
+    "height" : "+" + DEFAULT_UNIT * 2,
+  }),
+  decreaseHeight: slate.operation("resize", {
+    "width" : "+0",
+    "height" : "-" + DEFAULT_UNIT * 2,
+  }),
+  increaseWidth: slate.operation("resize", {
+    "width" : "+" + DEFAULT_UNIT * 2,
+    "height" : "+0",
+  }),
+  decreaseWidth: slate.operation("resize", {
+    "width" : "-" + DEFAULT_UNIT * 2,
+    "height" : "+0",
+  }),
+  moveToCenter: slate.operation("move", {
+    "x" : "(screenOriginX+screenSizeX/2)-(windowSizeX/2)",
+    "y" : "(screenOriginY+screenSizeY/2)-(windowSizeY/2)",
+    "width" : "windowSizeX",
+    "height" : "windowSizeY",
+  }),
+  moveAndResizeToCenter: slate.operation("move", {
+    "x" : "screenOriginX+screenSizeX/10",
+    "y" : "screenOriginY+screenSizeY/10",
+    "width" : "screenSizeX*0.8",
+    "height" : "screenSizeY*0.8",
+  }),
+  fullScreen: slate.operation("move", {
+    "x" : "screenOriginX",
+    "y" : "screenOriginY",
+    "width" : "screenSizeX",
+    "height" : "screenSizeY",
+  }),
+  moveToNextScreen: slate.operation("throw", {
+    "screen" : getNextScreenID,
+    "x" : "(screenOriginX+screenSizeX/2)-(min({windowSizeX,screenSizeX})/2)",
+    "y" : "(screenOriginY+screenSizeY/2)-(min({windowSizeY,screenSizeY})/2)",
+    "width" : "min({windowSizeX,screenSizeX})",
+    "height" : "min({windowSizeY,screenSizeY})",
+  }),
+};
+
+/*
+ * Bind Keys
+ */
+
+// Move focus: Hyper + HJKL
+slate.bind(withHyperKey('j'), slate.operation("focus", { "direction" : "down" }), true);
+slate.bind(withHyperKey('k'), slate.operation("focus", { "direction" : "up" }), true);
+slate.bind(withHyperKey('h'), slate.operation("focus", { "direction" : "left" }), true);
+slate.bind(withHyperKey('l'), slate.operation("focus", { "direction" : "right" }), true);
+
+// Move window: Hyper + Option + HJKL
+slate.bind(withHyperKey('j', true), function (win) {
+  win.doOperation(logStep);
+  win.doOperation(operations.moveDown);
+}, true);
+slate.bind(withHyperKey('k', true), function (win) {
+  win.doOperation(logStep);
+  win.doOperation(operations.moveUp);
+}, true);
+slate.bind(withHyperKey('h', true), function (win) {
+  win.doOperation(logStep);
+  win.doOperation(operations.moveLeft);
+}, true);
+slate.bind(withHyperKey('l', true), function (win) {
+  win.doOperation(logStep);
+  win.doOperation(operations.moveRight);
+}, true);
+
+// Resize window: Hyper + Option + NM,.
+slate.bind(withHyperKey('m', true), function (win) {
+  win.doOperation(logStep);
+  win.doOperation(slate.operation("sequence", {
+    "operations" : [
+      operations.increaseHeight,
+      operations.moveUp,
+    ],
   }));
 }, true);
-slate.bind('.:'+hyperKey+';'+optionKey, function(win) {
+slate.bind(withHyperKey(',', true), function (win) {
   win.doOperation(logStep);
-  win.doOperation(slate.operation("resize", {
-    "width" : "+40",
-    "height" : "+0"
-  }));
-  win.doOperation(slate.operation("nudge", {
-    "x" : "-20",
-    "y" : "+0"
+  win.doOperation(slate.operation("sequence", {
+    "operations" : [
+      operations.decreaseHeight,
+      operations.moveDown,
+    ],
   }));
 }, true);
-slate.bind('n:'+hyperKey+';'+optionKey, function(win) {
+slate.bind(withHyperKey('n', true), function (win) {
   win.doOperation(logStep);
-  win.doOperation(slate.operation("resize", {
-    "width" : "-40",
-    "height" : "+0"
-  }));
-  win.doOperation(slate.operation("nudge", {
-    "x" : "+20",
-    "y" : "+0"
+  win.doOperation(slate.operation("sequence", {
+    "operations" : [
+      operations.decreaseWidth,
+      operations.moveRight,
+    ],
   }));
 }, true);
-slate.bind('[:'+hyperKey+';'+optionKey, function(win) {
+slate.bind(withHyperKey('.', true), function (win) {
   win.doOperation(logStep);
-  win.doOperation(pushLeft);
+  win.doOperation(slate.operation("sequence", {
+    "operations" : [
+      operations.increaseWidth,
+      operations.moveLeft,
+    ],
+  }));
 }, true);
-slate.bind(']:'+hyperKey+';'+optionKey, function(win) {
+
+// Layout window: Hyper + Option + o0-
+slate.bind(withHyperKey('o', true), function (win) {
   win.doOperation(logStep);
-  win.doOperation(pushRight);
+  win.doOperation(operations.moveToCenter);
 }, true);
-slate.bind('o:'+hyperKey+';'+optionKey, function(win) {
+slate.bind(withHyperKey('0', true), function (win) {
   win.doOperation(logStep);
-  win.doOperation(moveAndResizeToCenter);
+  win.doOperation(operations.moveAndResizeToCenter);
 }, true);
-slate.bind('0:'+hyperKey+';'+optionKey, function(win) {
+slate.bind(withHyperKey('-', true), function (win) {
   win.doOperation(logStep);
-  win.doOperation(moveToCenter);
+  win.doOperation(operations.fullScreen);
 }, true);
-slate.bind('-:'+hyperKey+';'+optionKey, function(win) {
+slate.bind(withHyperKey('p', true), function (win) {
   win.doOperation(logStep);
-  win.doOperation(fullscreen);
+  win.doOperation(operations.moveToNextScreen);
 }, true);
-slate.bind('p:'+hyperKey+';'+optionKey, function(win) {
-  win.doOperation(logStep);
-  var thisScreen = slate.screen;
-  var thisWindow = slate.window;
-  var nextScreen;
-  if (slate.screenCount() > (slate.screen().id()+1)) {
-    nextScreenID = (slate.screen().id()+1);
-    nextScreen = slate.screenForRef(nextScreenID);
-  } else {
-    nextScreenID = 0;
-    nextScreen = slate.screenForRef(0);
+
+// Undo
+slate.bind(withHyperKey('u', true), function (win) {
+  win.doOperation(stepBack);
+}, true);
+
+/*
+ * Events
+ */
+
+slate.on("appActivated", function (event, app) {
+  // Hide Franz when lose focus
+  if (app.name() != 'Franz' && app.name() != 'Flycut') {
+    slate.operation("hide", {
+      "app" : 'Franz',
+    }).run();
   }
-  x = win.topLeft().x * (nextScreen.visibleRect().width/slate.screen().visibleRect().width);
-  y = win.topLeft().y * (nextScreen.visibleRect().height/slate.screen().visibleRect().height);
-  width = win.size().width * (nextScreen.visibleRect().width/slate.screen().visibleRect().width);
-  height = win.size().height * (nextScreen.visibleRect().height/slate.screen().visibleRect().height);
-  var throwNextResize = slate.operation("move", {
-    "screen" : nextScreen,
-    "x" : x.toString(),
-    "y" : y.toString(),
-    "width" : width.toString(),
-    "height" : height.toString()
-  });
-  var throwNext = slate.operation("throw", {
-    "screen" : nextScreen,
-  });
-  var nudge = slate.operation("nudge", {
-    "x" : '+'+x.toString(),
-    "y" : '+'+y.toString()
-  });
-  win.doOperation(throwNext);
-  // win.doOperation(nudge);
-  win.resize({
-    "width" : width.toString(),
-    "height" : height.toString()
-  });
-}, false);
-slate.bind('g:'+hyperKey+';'+optionKey, grid44);
-// undo
-slate.bind('u:'+hyperKey+';'+optionKey, stepBack);
+});
