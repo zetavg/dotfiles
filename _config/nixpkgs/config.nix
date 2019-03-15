b760a3cfe3db681cfd4ca06fe59a311ecce03a36
zetavg <mail@zeta.vg>
2019-03-15 16:12:16 +0800

    update nixpkgs config

---
+++ _config/nixpkgs/config.nix
@@ -0,0 +1,32 @@
+{
+  allowUnfree = true;
+  packageOverrides = pkgs: {
+    gdrive = pkgs.gdrive.overrideAttrs (oldAttrs: {
+      # Allow gdrive to use custom client credentials passed via environment variables
+      # (GDRIVE_CLIENT_ID, GDRIVE_CLIENT_SECRET) to avoid rate limit errors
+      patches = [
+        (builtins.toFile "patch" ''
+        --- ./handlers_drive.go
+        +++ ./handlers_drive.go
+        @@ -13,8 +13,15 @@
+          "time"
+         )
+
+        -const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
+        -const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+        +func getEnv(key, fallback string) string {
+        +  if value, ok := os.LookupEnv(key); ok {
+        +    return value
+        +  }
+        +  return fallback
+        +}
+        +
+        +var ClientId = getEnv("GDRIVE_CLIENT_ID", "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com")
+        +var ClientSecret = getEnv("GDRIVE_CLIENT_SECRET", "1qsNodXNaWq1mQuBjUjmvhoO")
+         const TokenFilename = "token_v2.json"
+         const DefaultCacheFileName = "file_cache.json"
+        '')
+      ];
+    });
+  };
+}
