ce8fa8413dd54214087667eb3dfdf906523df129
zetavg <mail@zeta.vg>
2021-10-16 12:27:52 +0800

    [JXA] update Toggle Dark Mode script, since apps will now follow system settings

--- mac/JXA/Toggle Dark Mode.js
+++ mac/JXA/Toggle Dark Mode.js
@@ -1,25 +1,9 @@
 function enableLightMode() {
   Application('System Events').appearancePreferences.darkMode = false
-
-  try {
-    Application('Alfred 3').setTheme('System Light')
-  } catch (e) {}
-
-  try {
-    Application('OmniFocus').preferences.byId('OFIColorPaletteIdentifier').value = 'default'
-  } catch (e) {}
 }
 
 function enableDarkMode() {
   Application('System Events').appearancePreferences.darkMode = true
-
-  try {
-    Application('Alfred 3').setTheme('System Dark')
-  } catch (e) {}
-
-  try {
-    Application('OmniFocus').preferences.byId('OFIColorPaletteIdentifier').value = 'builtin:20-Dark'
-  } catch (e) {}
 }
 
 (() => {
