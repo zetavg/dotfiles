3329c5d5f8b58379b7f47da3e579b118b7921029
zetavg <mail@zeta.vg>
2025-08-31 20:57:20 +0800

    (karabiner): disable keyboard-brightness-control

--- _config/karabiner/src/rules/keyboard-brightness-control.mjs
+++ _config/karabiner/src/rules/keyboard-brightness-control.mjs
@@ -1,39 +1,41 @@
 export default {
   description: 'Keyboard brightness/illumination control',
   manipulators: [
-    {
-      from: {
-        key_code: 'f1',
-        modifiers: {
-          mandatory: ['left_command'],
-        },
-      },
-      to: [
-        {
-          halt: false,
-          key_code: 'illumination_decrement',
-          lazy: true,
-          repeat: true,
-        },
-      ],
-      type: 'basic',
-    },
-    {
-      from: {
-        key_code: 'f2',
-        modifiers: {
-          mandatory: ['left_command'],
-        },
-      },
-      to: [
-        {
-          halt: false,
-          key_code: 'illumination_increment',
-          lazy: true,
-          repeat: true,
-        },
-      ],
-      type: 'basic',
-    },
+    // Disabled - use function-key-combinations instead
+    // {
+    //   from: {
+    //     key_code: 'f1',
+    //     modifiers: {
+    //       mandatory: ['left_command'],
+    //     },
+    //   },
+    //   to: [
+    //     {
+    //       halt: false,
+    //       key_code: 'illumination_decrement',
+    //       lazy: true,
+    //       repeat: true,
+    //     },
+    //   ],
+    //   type: 'basic',
+    // },
+    // {
+    //   from: {
+    //     key_code: 'f2',
+    //     modifiers: {
+    //       mandatory: ['left_command'],
+    //     },
+    //   },
+    //   to: [
+    //     {
+    //       halt: false,
+    //       key_code: 'illumination_increment',
+    //       lazy: true,
+    //       repeat: true,
+    //     },
+    //   ],
+    //   type: 'basic',
+    // },
+    {}
   ],
 };
